63466b229a43152eed04645841d4714ea0fba2db
Paulo Barbosa <paulohjbarbosa@gmail.com>
2022-11-10 18:53:56 -0300

    testing first rest in bookcontroller

---
+++ backend/src/main/java/com/paulobarbosa/foxlibrary/FoxlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.paulobarbosa.foxlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxlibraryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paulobarbosa/foxlibrary/FoxlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulobarbosa.foxlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
