8ec6179df4a92e0b741e44815a0d02d4206eaa55
Paulo Barbosa <paulohjbarbosa@gmail.com>
2022-11-26 21:59:27 -0300

    create class Format

---
+++ backend/src/main/java/com/paulobarbosa/foxlibrary/book/model/Format.java
@@ -0,0 +1,39 @@
+package com.paulobarbosa.foxlibrary.book.model;
+
+import com.paulobarbosa.foxlibrary.core.util.Constants;
+import com.paulobarbosa.foxlibrary.global.entity.EntityBase;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Format extends EntityBase {
+    @NotEmpty(message = Constants.REQUIRED_FILED)
+    @Size(max = 8)
+    @Column(name = "name", length = 8, nullable = false)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Format format = (Format) o;
+        return getId() != null && Objects.equals(getId(), format.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
